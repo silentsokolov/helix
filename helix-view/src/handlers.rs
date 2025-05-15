459552ccc908845190077a1a05618d6b9ee03748
Dmitriy Sokolov <silentsokolov@gmail.com>
2025-05-15 19:53:54 +0300

    copilot

--- helix-view/src/handlers.rs
+++ helix-view/src/handlers.rs
@@ -22,6 +22,7 @@ pub struct Handlers {
     pub signature_hints: Sender<lsp::SignatureHelpEvent>,
     pub auto_save: Sender<AutoSaveEvent>,
     pub document_colors: Sender<lsp::DocumentColorsEvent>,
+    pub copilot: Option<Sender<lsp::CopilotRequestCompletionEvent>>,
 }
 
 impl Handlers {
